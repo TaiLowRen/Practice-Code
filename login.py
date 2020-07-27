e27db4a619efcf47d1cab20c34ee5d0267d6e3cc
TaiLowRen <farawayplanet9090@gmail.com>
2020-07-27 15:57:19 -0400

    FINAL

--- login.py
+++ login.py
@@ -1,10 +1,10 @@
-selection = input("Enter 1 to login, 2 to make account: ")
+selection = input("Enter 1 to login, 2 to make account: ") #takes user input
 if selection == "1": #Login
-    username = input("Enter username:")
+    username = input("Enter username:") #takes user input
     password = input("Enter password:")
     f = open('passwords.txt')
     Flag = 0 
-    for i in f.read().split('\n'):
+    for i in f.read().split('\n'): #iterates through the passwords.txt file, checks if the password entered has been saved to the passwords.txt file
         if password == i:
             Flag = 1
     if Flag == 1:
@@ -13,11 +13,11 @@ if selection == "1": #Login
         print("Incorrect Password")
 
 elif selection == "2": #Creation
-    username = input("Enter new username:")
+    username = input("Enter new username:")#takes user input
     password = input("Enter new password:")
-    f = open("usernames.txt", "r")
+    f = open("usernames.txt", "r") 
     Flag = 0 
-    for i in f.read().split('\n'):
+    for i in f.read().split('\n'): #iterated through the usernames.txt file, checks if the username is taken
         if username == i:
             Flag = 1
     if Flag == 1:
